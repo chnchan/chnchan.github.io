57b6d40e1849b8c00baa193789110ef2422a4c0b
Hin Chan <32475946+chnchan@users.noreply.github.com>
2019-06-26 15:58:25 -0700

    Build files

---
+++ precache-manifest.2081ea4dd3048a87850cf1e1ba4cc573.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb0abc533b6b83fa44f89432160525c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e870d1fd05ef69c9aa7",
+    "url": "/static/css/main.e20e4e77.chunk.css"
+  },
+  {
+    "revision": "1719430fdb182a9512c6",
+    "url": "/static/js/2.53ab7b79.chunk.js"
+  },
+  {
+    "revision": "0e870d1fd05ef69c9aa7",
+    "url": "/static/js/main.9aa24a49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df7c833a16359abef62c98a929959216",
+    "url": "/static/media/CoursesWEB.df7c833a.pdf"
+  },
+  {
+    "revision": "588c1bc237a42a4988b0077917efdb18",
+    "url": "/static/media/ResumeWEB.588c1bc2.pdf"
+  },
+  {
+    "revision": "d2dd228c9734ec540def6aaa66f33e3f",
+    "url": "/static/media/chun-hin-chan2.d2dd228c.jpg"
+  },
+  {
+    "revision": "f5b7ca733fe5ad6cddae492a076c1d52",
+    "url": "/static/media/email.f5b7ca73.svg"
+  },
+  {
+    "revision": "156a46ef9944de74981d64cb8c5ee7e0",
+    "url": "/static/media/facebook.156a46ef.svg"
+  },
+  {
+    "revision": "23fc8f81f92bb2981d8f9e089d7df14a",
+    "url": "/static/media/github.23fc8f81.svg"
+  },
+  {
+    "revision": "7cc0a741314ca4d8859b39fd0a19c63c",
+    "url": "/static/media/linkedin.7cc0a741.svg"
+  },
+  {
+    "revision": "4f5adb7066d481e3873c23f058ac1712",
+    "url": "/static/media/logo.4f5adb70.png"
+  }
+]);
